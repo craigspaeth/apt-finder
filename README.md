8d2a481984e73a0085994caa0f81148c466e27e0
Artsy <craigspaeth@gmail.com>
2017-07-30 22:00:05 -0400

    All hooked up

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # apt-finder
-Finding an apartment
+
+Finding an apartment with Nightmare.js.
+
+```
+npm i
+npm run scrape
+```
+
+## Scraping
+
+- [x] Nooklyn
+- [x] Compass
+- [x] My Space NYC
+- [x] Naked Apartments
+- [x] Zumper
+- [x] RentHop
+- [x] Tripple Mint
+- [x] Craigslist
+- [x] NYBits
+- [x] Trulia
+- [] ApartmentList (No search filters)
+- [] Urban Edge (Uses JSON endpoint)
+- [] LeaseBreak (Uses client-side POST for HTML)
+
+# Buildings
+
+- https://ny.curbed.com/2016/7/14/12187804/pacific-park-brooklyn-535-carlton-affordable-housing
+- https://www.common.com
+- https://www.thebrooklynzinc.com/
--- index.js
+++ index.js
@@ -1,48 +1,110 @@
-const Nightmare = require('nightmare')
-const pmongo = require('promised-mongo')
-const accounting = require('accounting')
-const cheerio = require('cheerio')
 const moment = require('moment')
-const _ = require('lodash')
+const scrape = require('./lib/scrape')
+const mail = require('./lib/mail')
+const db = require('./lib/db')
 
-let { BEDS, MAX_RENT, DEBUG, DB_URL, TIMEOUT } = process.env
-DEBUG = Boolean(DB_URL)
-TIMEOUT = Number(TIMEOUT)
-const STREET_EASY_URL = `http://streeteasy.com/for-rent/prospect-heights/price:-${MAX_RENT}%7Cbeds:${BEDS}`
+const { BEDS, MAX_RENT } = process.env
 
-const db = pmongo(DB_URL, ['listings'])
-const nightmare = Nightmare({
-  show: DEBUG,
-  waitTimeout: TIMEOUT,
-  gotoTimeout: TIMEOUT,
-  loadTimeout: TIMEOUT,
-  executionTimeout: TIMEOUT
-})
+const scraperConfig = [
+  {
+    url: `https://nooklyn.com/rentals?q=eyJhZGRyZXNzIjpudWxsLCJhZ2VudF9pZF9saXN0IjpudWxsLCJhbWVuaXR5X2xpc3QiOm51bGwsImFwYXJ0bWVudCI6bnVsbCwiZGF0ZV9hdmFpbGFibGUiOm51bGwsIm1pbl9iYXRocyI6bnVsbCwiYmVkX2xpc3QiOiIyIiwiYm90dG9tX3JpZ2h0X2xpc3QiOiIiLCJleGNsdXNpdmUiOmZhbHNlLCJmZWF0dXJlZCI6ZmFsc2UsImxpc3RpbmdfaWQiOm51bGwsIm1heF9wcmljZSI6MzAwMCwibWluX3ByaWNlIjpudWxsLCJtaW5fcHJpY2VfcGVyX2JlZCI6bnVsbCwibWF4X3ByaWNlX3Blcl9iZWQiOm51bGwsImxhdGl0dWRlIjpudWxsLCJsb25naXR1ZGUiOm51bGwsIm5laWdoYm9yaG9vZF9pZF9saXN0IjoiOCIsIm5vX2ZlZSI6ZmFsc2UsIm9yZGVyIjoicmVjZW50bHlfY3JlYXRlZCIsIm93bmVyX3BheXMiOmZhbHNlLCJwZXRzIjoiQW55IiwibWluX3NxdWFyZV9mZWV0IjpudWxsLCJtYXhfc3F1YXJlX2ZlZXQiOm51bGwsInN0YXR1c19saXN0IjoiQXZhaWxhYmxlIiwic3Vid2F5X2lkX2xpc3QiOiIiLCJ0b3BfbGVmdF9saXN0IjoiIiwidHlwZSI6InJlc2lkZW50aWFsIiwidHJhbnNhY3Rpb24iOiJyZW50YWxzIiwicmVnaW9uX2lkX2xpc3QiOm51bGwsInZhY2FudCI6ZmFsc2V9`,
+    liSelector: '.nklyn-listing',
+    link: $li => $li.find('a'),
+    thumbnail: $li => $li.find('img'),
+    rent: $li => $li.find('.nooklyn_listing_square_price'),
+    noFee: $li => $li.find('.nooklyn_listing_square_fee').length
+  },
+  {
+    url: `https://www.compass.com/search/rentals/nyc/prospect-heights/?bedrooms=${BEDS}&max_price=${MAX_RENT}`,
+    liSelector: 'uc-listing-card',
+    link: $li => $li.find('.uc-listingCard-titleWrapper'),
+    thumbnail: $li => $li.find('.uc-listingCard-image'),
+    rent: $li => $li.find('[data-tn="listingCard-label-price"]'),
+    noFee: $li => $li.find('.uc-listingCard-cornerBox').text().match(/no fee/i)
+  },
+  {
+    url: `http://streeteasy.com/for-rent/prospect-heights/price:-${MAX_RENT}%7Cbeds:${BEDS}`,
+    liSelector: '#result-details .item:not(.featured)',
+    link: $li => $li.find('a[href*="/building"]'),
+    thumbnail: $li => $li.find('.photo img'),
+    rent: $li => $li.find('.price'),
+    noFee: $li => $li.find('.no_fee').length
+  },
+  {
+    url: `http://www.myspace-nyc.com/?layout=${BEDS}br&max_price=${MAX_RENT}&neighborhoods=166&page=1&sort=updated&view=map`,
+    liSelector: '.listing ',
+    link: $li => $li.find('a'),
+    thumbnail: $li => $li.find('img'),
+    rent: $li => $li.find('.price'),
+    noFee: $li => false
+  },
+  {
+    url: `https://www.nakedapartments.com/renter/listings/search?bldid=&nids=28&aids=${BEDS * 2}&min_rent=&max_rent=${MAX_RENT}&amids=&pets=&baths=&oh=&subl=&button=`,
+    liSelector: '.listing-row',
+    link: $li => $li.find('.listing-row__image-link'),
+    thumbnail: $li => $li.find('.listing-row__image-link img'),
+    rent: $li => $li.find('.listing-row__rent'),
+    noFee: $li => $li.find('.offer').text().match(/no fee/i)
+  },
+  {
+    url: `https://www.zumper.com/apartments-for-rent/new-york-ny/prospect-heights/${BEDS}-beds/under-${MAX_RENT}?box=-73.97743,40.67170,-73.96261,40.68403`,
+    liSelector: '.listingFeed [ng-repeat*=item]',
+    link: $li => $li.find('[ng-href*="/apartments-for-rent"]'),
+    thumbnail: $li => $li.find('.feedItem-imgBox img'),
+    rent: $li => $li.find('[ng-bind="::item.priceText"]'),
+    noFee: $li => $li.find('.feedItem-status').text().match(/no broker fee/i)
+  },
+  {
+    url: `https://www.renthop.com/search/nyc?location_search=&bedrooms%5B%5D=${BEDS}&min_price=0&max_price=${MAX_RENT}&q=&neighborhoods_str=78&sort=hopscore&page=1&search=1`,
+    liSelector: '.search-listing td',
+    link: $li => $li.find('a[href*="listings"]'),
+    thumbnail: $li => $li.find('.search-photo-first img'),
+    rent: $li => $li.find('[id*="listing"]'),
+    noFee: $li => $li.find('.listing-search-info').text().match(/no fee/i)
+  },
+  {
+    url: `https://www.triplemint.com/listings?listing_type=rental&max_price=${MAX_RENT}&neighborhood=78&bedrooms=${BEDS}`,
+    liSelector: '.listing-card',
+    link: $li => `<a/>${$li.find('.thumbnail').css('background-image')}</a>`,
+    thumbnail: $li => $li.find('.thumbnail'),
+    rent: $li => $li.find('.price'),
+    noFee: $li => false
+  },
+  {
+    url: `https://newyork.craigslist.org/search/abo?query=prospect+heights&max_price=${MAX_RENT}&min_bedrooms=${BEDS}&availabilityMode=0`,
+    liSelector: '.result-row',
+    link: $li => $li.find('> a'),
+    thumbnail: $li => $li.find('img'),
+    rent: $li => $li.find('.result-price').first(),
+    noFee: $li => true
+  },
+  {
+    url: `https://www.nybits.com/search/?_a%21process=y&_rid_=5&_ust_todo_=65733&_xid_=lb..I_O3aR5Fr0-1501456916&%21%21atypes=${BEDS}br&%21%21rmin=&%21%21rmax=${MAX_RENT}&%21%21fee=nofee&%21%21orderby=dateposted&%21%21nsearch=prospect_heights&submit=+SHOW+RENTAL+APARTMENTS+`,
+    liSelector: '#rentaltable tr',
+    link: $li => $li.find('a'),
+    thumbnail: $li => $li,
+    rent: $li => $li.find('.lst_sr_price'),
+    noFee: $li => true
+  },
+  {
+    url: `https://www.trulia.com/for_rent/5210_nh/${BEDS}p_beds/0-${MAX_RENT}_price/beds;d_sort/`,
+    liSelector: '.card',
+    link: $li => $li.find('a').first(),
+    thumbnail: $li => $li.find('.cardPhoto'),
+    rent: $li => $li.find('.cardPrice'),
+    noFee: $li => false
+  }
+]
 
 const main = async () => {
-  const listings = _.flatten(await Promise.all([scrapeStreetEasy()]))
-  console.log(listings)
-}
-
-const scrapeStreetEasy = async () => {
-  const html = await nightmare
-    .goto(STREET_EASY_URL)
-    .wait(() => {
-      !!document.body.querySelector('#result-details')
-    })
-    .evaluate(() => document.body.innerHTML)
-    .end()
-  const $ = cheerio.load(html)
-  const listings = $('#result-details .item:not(.featured)')
-    .map((i, el) => ({
-      uid: $(el).attr('id'),
-      thumbnail: $(el).find('.photo img').attr('src'),
-      rent: accounting.unformat($(el).find('.price').text()),
-      noFee: $(el).find('.no_fee').length,
-      scrapedOn: moment().format()
-    }))
-    .get()
-  return listings
+  await scrape(scraperConfig)
+  const listings = await db.listings.find({
+    scrapedOn: {
+      $gte: moment().subtract(5, 'minutes').toDate()
+    }
+  })
+  if (listings.length) await mail(listings)
+  process.exit()
 }
 
 main().catch(console.error)
